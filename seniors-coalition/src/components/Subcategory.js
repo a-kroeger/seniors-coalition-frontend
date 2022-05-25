63c6f5d4013c6f0825d2280a981a260de9625d36
Alexander Kroeger <alex@detourdigital.ca>
2022-05-24 23:37:58 -0600

    fixed inappropriate 404 error triggering

--- seniors-coalition/src/components/Subcategory.js
+++ seniors-coalition/src/components/Subcategory.js
@@ -8,18 +8,19 @@ function SubCategory(props) {
     const title = props.subCategories.attributes.Title
     const description = props.subCategories.attributes.Description
     const subCategory = props.subCategories.attributes.Url
+    const childLinks = props.subCategories.attributes.items.data.length
     const { url } = useRouteMatch();
 
     return (
         <div className="subcategory">
             <h3>{title}</h3>
             <ReactMarkdown children={description} />
-            <Link
+            {childLinks > 0 && <Link
               style={{background: props.theme}}
               className="button"
               to={`${url}/${subCategory}`} replace >
                 Learn More
-            </Link>
+            </Link>}
         </div>
     )
 }
